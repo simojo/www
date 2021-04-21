6f954c201fe7e3db5fcb2adb020cd99c1044036f
Simon Jones <github@simonjjones.com>
2021-04-21 11:40:27 -0400

    backend changes

--- backend/helpers.py
+++ backend/helpers.py
@@ -1,13 +1,13 @@
 import json
 import os
 import re
-# https://www.w3schools.com/python/python_regex.asp
 
 POSTS = "./data/posts/"
 
 class Data:
     @staticmethod
-    def fetchPostsJson():
+    def fetchPosts():
+        result = []
         for path in os.listdir(POSTS):
             txt = open(POSTS + path).read()
             blob = lambda x: f"(?<=^{x} ).*"
@@ -15,9 +15,28 @@ class Data:
             subtitle = re.findall(blob("##"), txt, re.MULTILINE)[0]
             date = re.findall(blob("###"), txt, re.MULTILINE)[0]
             body = re.sub(r"^(#.*)\n", "", txt, flags=re.MULTILINE).lstrip().rstrip()
-            print(json.dumps({
+            route = re.findall("(?=\.md)", path)[0]
+            result.append({
+                "route": route,
                 "title": title,
                 "subtitle": subtitle,
                 "date": date,
                 "body": body
-            }))
+            })
+        return result
+
+    @staticmethod
+    def fetchPostsJson():
+        return json.dumps(Data.fetchPosts())
+
+    @staticmethod
+    def fetchPostJson(postroute):
+        return json.dumps(list(filter(lambda x: x["route"] == postroute, Data.fetchPosts()))[0])
+
+    @staticmethod
+    def fetchPostsLandingJson():
+        # FIXME do not capture variable
+        return json.dumps(list(map(lambda x: x.pop("body"), Data.fetchPosts())))
+
+def OK(str=""):
+    return str,200
