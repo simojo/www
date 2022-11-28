0043f754f2446f12a381b4c8355aa371f1e5d5c5
Simon Jones <github@simonjjones.com>
2022-11-28 12:47:35 -0500

    trying to get multiline exprs

--- src/Postpage.svelte
+++ src/Postpage.svelte
@@ -7,47 +7,48 @@
   import katex from "katex";
   // custom marked blurb (https://gist.github.com/tajpure/47c65cf72c44cb16f3a5df0ebc045f2f)
   const renderer = new marked.Renderer();
-let originParagraph = renderer.paragraph.bind(renderer);
-renderer.paragraph = (text) => {
-  const blockRegex = /\$\$[^\$]*\$\$/g;
-  const inlineRegex = /\$[^\$]*\$/g;
-  let blockExprArray = text.match(blockRegex);
-  let inlineExprArray = text.match(inlineRegex);
-  for (let i in blockExprArray) {
-    const expr = blockExprArray[i];
-    const result = renderMathsExpression(expr);
-    text = text.replace(expr, result);
+  let originParagraph = renderer.paragraph.bind(renderer);
+  renderer.paragraph = (text) => {
+    const blockRegex = /\$\$[^\$]*\$\$/g;
+    const inlineRegex = /\$[^\$]*\$/g;
+    let blockExprArray = text.match(blockRegex);
+    let inlineExprArray = text.match(inlineRegex);
+    for (let i in blockExprArray) {
+      const expr = blockExprArray[i];
+      const result = renderMathsExpression(expr);
+      text = text.replace(expr, result);
+    }
+    for (let i in inlineExprArray) {
+      const expr = inlineExprArray[i];
+      const result = renderMathsExpression(expr);
+      text = text.replace(expr, result);
+    }
+    return originParagraph(text);
   }
-  for (let i in inlineExprArray) {
-    const expr = inlineExprArray[i];
-    const result = renderMathsExpression(expr);
-    text = text.replace(expr, result);
-  }
-  return originParagraph(text);
-}
-function renderMathsExpression (expr) {
-  if (expr[0] === '$' && expr[expr.length - 1] === '$') {
-    let displayStyle = false;
-    expr = expr.substr(1, expr.length - 2);
+  function renderMathsExpression (expr) {
+    console.log(expr);
     if (expr[0] === '$' && expr[expr.length - 1] === '$') {
-      displayStyle = true;
+      let displayStyle = false;
       expr = expr.substr(1, expr.length - 2);
+      if (expr[0] === '$' && expr[expr.length - 1] === '$') {
+        displayStyle = true;
+        expr = expr.substr(1, expr.length - 2);
+      }
+      let html = null;
+      try {
+        html = katex.renderToString(expr);
+      } catch (e) {
+        console.error(e);
+      }
+      if (displayStyle && html) {
+        html = html.replace(/class="katex"/g, 'class="katex katex-block" style="display: block;"');
+      }
+      return html;
+    } else {
+      return null;
     }
-    let html = null;
-    try {
-      html = katex.renderToString(expr);
-    } catch (e) {
-      console.error(e);
-    }
-    if (displayStyle && html) {
-      html = html.replace(/class="katex"/g, 'class="katex katex-block" style="display: block;"');
-    }
-    return html;
-  } else {
-    return null;
   }
-}
-marked.setOptions({renderer: renderer});
+  marked.setOptions({renderer: renderer});
   /*
   const options = {
     displayMode: true,
